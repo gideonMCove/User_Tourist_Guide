5b64fca72105df8b316836023ebc5fae5607e8e5
GideonCove <gideoncove@gmail.com>
2024-06-03 18:19:28 -0400

    clean!

--- server.js
+++ server.js
@@ -17,17 +17,9 @@ app.listen(PORT, () => {
 })
 
 
-
-
 const spotController = require('./controllers/spotController')
 const regionController = require('./controllers/regionController')
 const countryController = require('./controllers/countryController')
-
-
-
-
-
-
 
 
 app.get('/', (req, res) => res.send('This is our landing page!'))
@@ -37,26 +29,22 @@ app.get('/spots/:id', spotController.getSpotById)
 app.post('/spots', spotController.createSpot)
 app.put('/spots/:id', spotController.updateSpot)
 app.delete('/spots/:id', spotController.deleteSpot)
-app.get('/spots/items/:searchTerm', spotController.getSpotBySpotName )
+app.get('/spots/search/:searchTerm', spotController.getSpotBySpotName )
 
 app.get('/regions', regionController.getAllRegions)
 app.get('/regions/:id', regionController.getRegionById)
 app.post('/regions', regionController.createRegion)
 app.put('/regions/:id', regionController.updateRegion)
 app.delete('/regions/:id', regionController.deleteRegion)
-app.get('/regions/items/:searchTerm', regionController.getRegionByRegionName )
+app.get('/regions/search/:searchTerm', regionController.getRegionByRegionName )
 
 app.get('/countrys', countryController.getAllCountrys)
 app.get('/countrys/:id', countryController.getCountryById)
 app.post('/countrys', countryController.createCountry)
 app.put('/countrys/:id', countryController.updateCountry)
 app.delete('/countrys/:id', countryController.deleteCountry)
-app.get('/countrys/items/:searchTerm', countryController.getCountryByCountryName )
+app.get('/countrys/search/:searchTerm', countryController.getCountryByCountryName )
 
 
 app.get('*', (req, res) => res.send('404 page not found'))
-
-
-
-
 
