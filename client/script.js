4f8a44c40e0afdda48176958283ccb3ada7b39ed
GideonCove <gideoncove@gmail.com>
2024-06-05 17:07:24 -0400

    front end works

--- client/script.js
+++ client/script.js
@@ -9,3 +9,102 @@
 //         res.status(500).json({ error: error.message })
 //     }
 // }
+
+
+
+
+// const countryEndpoint = `http://localhost:3001/countrys`
+// const regionEndpoint = 'http://localhost:3001/region'
+// const spotEndpoint = 'http://localhost:3001/spot'
+
+const countryEndpoint = `countrys`
+const regionEndpoint = 'http://localhost:3001/region'
+const spotEndpoint = 'http://localhost:3001/spot'
+const locationTest = document.querySelector('#location')
+
+const searchBar = document.getElementById('searchBar')
+const anchorCountry = document.getElementById('anchorCountry')
+const anchorRegion = document.getElementById('anchorRegion')
+const anchorSpot = document.getElementById('anchorSpot')
+const topImage = document.getElementById('topImage')
+const button = document.getElementById('test')
+
+const getCountries = async () => {
+    const countrys = await axios.get(`http://localhost:3001/countrys`)
+    console.log(countrys.data.message)
+}
+getCountries()
+
+
+
+button.addEventListener('click', async () => {
+    const res = await axios.get(`http://localhost:3001/${countryEndpoint}`)
+
+    
+    function setTestLocation()  {        
+            locations = res.data[0].location
+            document.getElementById('location').innerHTML = locations
+        }
+        setTestLocation()
+        // let getLocationTest = res.data.location
+        // document.getElementById('location').innerHTML = getLocationTest
+
+    }
+    // let data = country.data
+    
+    // console.log(res.data)
+    // let arrayCountry = []
+    // res.data.countrys.forEach(country => {
+    //     let location = res.data.countrys.location
+    //     arrayCountry.push(location)
+    //     const listCountry = document.getElementById('listCountry')
+    //     listCountry.textContent = arrayCountry
+        
+    // })
+    
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     const searchButton = document.getElementById('searchButton');
+    //     const searchInput = document.getElementById('searchInput');
+    //     const firearmsList = document.getElementById('firearms-list');
+    
+    //     searchButton.addEventListener('click', async () => {
+    //         const searchTerm = searchInput.value.trim();
+    
+    //         try {
+    //             let response;
+    //             if (searchTerm) {
+    //                 response = await axios.get(http://localhost:3001/firearms?name=${encodeURIComponent(searchTerm)});
+    //             } else {
+    //                 response = await axios.get(http://localhost:3001/firearms);
+    //             }
+    
+    //             firearmsList.innerHTML = '';
+    //             response.data.forEach(firearm => {
+    //                 const firearmItem = document.createElement('li');
+    //                 console.log(firearm);
+    //                 firearmItem.classList.add('firearmItem');
+    //                 firearmItem.innerHTML = 
+    //                     <h3>Name: ${firearm.name}</h3>
+    //                     <p>Caliber: ${firearm.caliber}</p>
+    //                     <p>Manufacturer: ${firearm.manufacturerId}</p>
+    //                     <p> Ownership:${firearm.Ownership}</p>
+    //                     <p>History: ${firearm.history}</p>
+    //                     <img src="${firearm.Image}" alt="Firearm Image"/>
+                        
+    //                 ;
+    //                 firearmsList.appendChild(firearmItem);
+    //             });
+    //         } catch (error) {
+    //             console.error('Error fetching data:', error);
+    //         }
+    //     });
+    // });
+
+
+
+    
+   
+
+    
+
+)
--- controllers/countryController.js
+++ controllers/countryController.js
@@ -80,3 +80,95 @@ module.exports = {
     deleteCountry,
     getCountryByCountryName
 }
+
+// const { Firearm } = require('../models');
+// const db = require('../db');
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+
+// const getAllFirearms = async (req, res) => {
+//   try {
+//     const searchTerm = req.query.name;
+//     let firearms;
+
+//     if (searchTerm) {
+//       firearms = await Firearm.find({ name: new RegExp(searchTerm, 'i') }); // case-insensitive search
+//     } else {
+//       firearms = await Firearm.find();
+//     }
+
+//     res.json(firearms);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// const getFirearmById = async (req, res) => {
+//   try {
+//     const firearm = await Firearm.findById(req.params.id);
+//     if (!firearm) {
+//       return res.status(404).json({ message: 'Firearm not found' });
+//     }
+//     res.json(firearm);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// const createFirearm = async (req, res) => {
+//   const firearm = new Firearm({
+//     name: req.body.name,
+//     caliber: req.body.caliber,
+//     manufacturerId: req.body.manufacturerId,
+//     history: req.body.history,
+//     image: req.body.image
+//   });
+
+//   try {
+//     const newFirearm = await firearm.save();
+//     res.status(201).json(newFirearm);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// };
+
+// const updateFirearm = async (req, res) => {
+//   try {
+//     const firearm = await Firearm.findById(req.params.id);
+//     if (!firearm) {
+//       return res.status(404).json({ message: 'Firearm not found' });
+//     }
+
+//     firearm.name = req.body.name;
+//     firearm.caliber = req.body.caliber;
+//     firearm.manufacturerId = req.body.manufacturerId;
+//     firearm.history = req.body.history;
+//     firearm.image = req.body.image;
+
+//     const updatedFirearm = await firearm.save();
+//     res.json(updatedFirearm);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// };
+
+// const deleteFirearm = async (req, res) => {
+//   try {
+//     const firearm = await Firearm.findById(req.params.id);
+//     if (!firearm) {
+//       return res.status(404).json({ message: 'Firearm not found' });
+//     }
+//     await firearm.remove();
+//     res.json({ message: 'Firearm deleted' });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// module.exports = { 
+//   getAllFirearms,
+//   getFirearmById,
+//   createFirearm,
+//   updateFirearm,
+//   deleteFirearm,
+// };
