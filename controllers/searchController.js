a3f0ec9db369f285d4216133d067babf8028df6f
GideonCove <gideoncove@gmail.com>
2024-06-06 10:08:01 -0400

    search function works

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -5,25 +5,51 @@ const Region = require('../models/region')
 
 
 const searchTourism = async (req, res) => {
-    // try {
+
+    try {
         const searchTerm = req.params.searchTerm
+
         let search = await Spot.find({ location: { $regex: searchTerm, $options: 'i'}})
-        res.json(search)
-        
-        if (res.status == 404)
-            res = ''
-            
+
+        if (search.length === 0) {
             search = await Region.find({ location: { $regex: searchTerm, $options: 'i'}})
-            res.json(search)
-            if (res.status == 404)
-                res = ''
-                search = await Country.find({ location: { $regex: searchTerm, $options: 'i'}})
-                res.json(search)
-    // } catch (error) {
-    //     return res.status(500).send(error.message)
-    // }
+        }
+
+        if (search.length === 0) {
+            search = await Country.find({ location: { $regex: searchTerm, $options: 'i'}})
+        }
+
+        res.json(search);
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
 }
 
 module.exports = {
     searchTourism
 }
+
+// if (res.status !=404 || res.status !=304){
+    // || res.status() !=res.status(304)
+
+//     const conditionalTest = res.status(404)
+//     try {
+//         const searchTerm = req.params.searchTerm
+//         let search = await Spot.find({ location: { $regex: searchTerm, $options: 'i'}})              
+//         if (res.status() != conditionalTest ){
+//             res.json(search)
+//         }else {
+//             let search = await Region.find({ location: { $regex: searchTerm, $options: 'i'}})
+//             if (res.status() != res.status(404) ){
+//                 res.json(search)
+//             }else {
+//                 let search = await Country.find ({ location: { $regex: searchTerm, $options: 'i'}})
+//                 if (res.status() != res.status(404) ){
+//                     res.json(search)
+//                 }
+//             }
+//         } 
+//     } catch (error) {
+//         return res.status(500).send(error.message)
+//     }
+// }
